d08326cb8086b42257a8acc0acbe3a7f2e41eab9
keisukesuzuki <ne201201@senshu-u.jp>
2022-01-12 04:41:55 +0000

    カート機能(トップからの遷移)

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,6 @@ class Product < ApplicationRecord
   belongs_to :mainmenu
   belongs_to :order, optional: true
 
-  has_many :product_toppings
+  has_many :product_toppings, dependent: :destroy #これによってproductを削除すると関連付けしているtoppingも消える(中間テーブルから)
   has_many :toppings, through: :product_toppings
 end
