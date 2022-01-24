0739a8e26660fdb4929c45fff3cf31ba5643f912
keisukesuzuki <ne201201@senshu-u.jp>
2022-01-24 22:44:48 +0000

    バリデーション最大値、最小値の修正

--- app/models/member.rb
+++ app/models/member.rb
@@ -2,12 +2,12 @@ class Member < ApplicationRecord
   has_secure_password
   has_many :orders, dependent: :destroy
 
-  validates :name, presence: true, length: {maximum:30, allow_blank: true }, uniqueness: true
-  validates :fullname, presence: true, length: {maximum:21, allow_blank: true }
+  validates :name, presence: true, length: {maximum:29, allow_blank: true }, uniqueness: true
+  validates :fullname, presence: true, length: {maximum:20, allow_blank: true }
   validates :address, presence: true
   validates :email, email: {allow_blank: true}, uniqueness: true
   validates :phone,presence: true, format: { with: /\A[0-9\-\(\)]*\z/, allow_blank: true }
-  validates :password, length: {minimum: 8, maximum:21, allow_blank: true },
+  validates :password, length: {minimum: 7, maximum:20, allow_blank: true },
     format: {
       with: /\A[A-Za-z][A-Za-z0-9]*\z/,
       allow_blank: true
